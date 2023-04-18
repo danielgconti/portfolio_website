213b055662692d86ca897499ae622eee6c7f469d
Daniel Conti <90418110+danielgconti@users.noreply.github.com>
2023-04-17 20:46:45 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include 'head.php';
+    ?>
+    
+
+    <title>Document</title>
+</head>
+<body>
+    <h1>Home</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include 'head.php';
+    ?>
+    <title>Home</title>
+</head>
+<body>
+    <div class="content-container">
+        <p class="intro-text">Hello, I'm Daniel Conti.<br>I'm a computer science student.</p>
+        <button><a href=home.php>View Website</a></button>
+    </div>
+</body>
+</html>
